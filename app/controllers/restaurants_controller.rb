fac4e303da846981450b8c2023ea72be3b0aaf0d
Stamatis Vogiatzopoulos <stamvogiatzop@gmail.com>
2024-01-24 16:47:33 +0100

    I hate this exercise.. Managed to complete only one user scenario.2

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,30 +4,11 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-    @restaurant = Restaurant.find(set_restaurant)
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
     @restaurant = Restaurant.new
     redirect_to restaurant_path(@restaurant)
   end
-
-  def create
-    @restaurant = Restaurant.new(restaurant_params)
-
-    if @restaurant.save
-      redirect_to @restaurant, notice: "Restaurant was successfully created."
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  private
-    def set_restaurant
-      @restaurant = Restaurant.find(params[:id])
-    end
-
-    def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :rating)
-    end
 end
